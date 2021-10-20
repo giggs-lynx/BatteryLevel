46d7b9d6b5bde80b94a34dafe2d97a5dfbb5db76
giggs <giggs.tasi@gmail.com>
2021-10-20 16:24:57 +0800

    Initial Commit

---
+++ BatteryLevel/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BatteryLevel
+//
+//  Created by giggs on 2021/10/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
